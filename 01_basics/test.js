4341c66b07f56b5dd093522f7536793230b164ab
twaritagrawal <103057344+twaritagrawal@users.noreply.github.com>
2024-02-23 10:29:18 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
-console.log("hello")
+let accountId=14258
+const accountName="twarit"
+// accountName="asdfghjk" we can't do this
+accountId=455852
+console.table([accountId,accountName])
+console.log(typeof accountId)
+console.log(typeof null)
