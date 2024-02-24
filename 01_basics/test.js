8e5b18010b3869516fc9ff828cb0bb91c665e8c8
twaritagrawal <103057344+twaritagrawal@users.noreply.github.com>
2024-02-24 09:49:58 +0000

    conversions

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -6,3 +6,17 @@ console.table([accountId,accountName])
 console.log(typeof accountId)
 console.log(typeof null)
 const ch=123
+// conversion operations
+
+// "33"=>33
+//"33abc"=>NaN
+
+let isLoggedIn="twarit"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1=>true; 0=>false
+// ""=>false
+//"hitesh"=>true
+let someNumber= 33
+let stringNumber=String(someNumber) //converted into strings
+console.log(stringNumber);
