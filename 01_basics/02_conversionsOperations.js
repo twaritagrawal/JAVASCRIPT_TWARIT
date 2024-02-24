8e515cedb65a941dfe69251be61b94eb3cbfed76
twaritagrawal <103057344+twaritagrawal@users.noreply.github.com>
2024-02-24 10:09:31 +0000

    conversions_tips

---
+++ 01_basics/02_conversionsOperations.js
@@ -0,0 +1,21 @@
+/*let score="dfghjkl"
+ 
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+// //NaN is also a type of number which prints out when a string is which is type of abc,sdfgh etc.
+//in case of null it printsout 0
+//true=>1; false=>0
+let isLoggedIn=""
+let booleanisLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof booleanisLoggedIn);
+console.log(booleanisLoggedIn);
+//conversions
+//1=>true ,0=>false
+// ""=>false
+//"hitesh"=>true
+
